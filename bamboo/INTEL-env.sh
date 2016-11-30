614914043496a329c47b9afb722685af8c4bd375
Willem Deconinck <willem.deconinck@ecmwf.int>
2016-11-30 18:08:28 +0000

    Use intel 16.0.3 for atlas 0.8.2

--- bamboo/INTEL-env.sh
+++ bamboo/INTEL-env.sh
@@ -7,7 +7,7 @@ module unload grib_api
 module unload emos
 module unload fftw
 module unload libemos
-module switch gnu intel/15.0.2
+module switch gnu intel/16.0.3
 
 # Load CMake 3.0.2 (ECBUILD-246)
 module load cmake/3.0.2
