7ae6c5aa32b801d61df0f6d96507e239d68b1ff7
Willem Deconinck <willem.deconinck@ecmwf.int>
2017-05-27 21:13:53 +0100

    Document fckit_main and fckit_log

--- src/fckit/module/fckit_log.F90
+++ src/fckit/module/fckit_log.F90
@@ -2,132 +2,109 @@ module fckit_log_module
 use fckit_object_module, only: fckit_object
 implicit none
 private
-
 public :: fckit_log
-public :: log
-public :: logchannel
-
+public :: fckit_logchannel
 private :: fckit_object
 
-type, extends(fckit_object) :: logchannel
+#include "fckit_log.inc"
+
+type, FORD_PRIVATE :: fckit_log_type
+  !! Private type of [[fckit_log_module:fckit_log(variable)]] module variable
+  !!
+  !! It wraps ```eckit::Log```, allowing Fortran and C++ code to log to the
+  !! same output channels
+  
+  integer :: SIMPLE = 0
+    !! Style for logging without any prefix
+  integer :: PREFIX = 1
+    !! Style for logging with prefix
+    !!
+    !!    (I) --> info
+    !!    (W) --> warning
+    !!    (E) --> error
+    !!    (D) --> debug
+
+  integer :: TIMESTAMP = 2
+    !! Style for logging with prefix that contains time stamp and taskID
+    !!
+    !!    <taskID> <TIME> (I) --> info
+    !!    <taskID> <TIME> (W) --> warning
+    !!    <taskID> <TIME> (E) --> error
+    !!    <taskID> <TIME> (D) --> debug
+
+contains
+
+  procedure, nopass, public :: debug
+    !! Log to debug channel
+
+  procedure, nopass, public :: info
+    !! Log to info channel
+
+  procedure, nopass, public :: warning
+    !! Log to warning channel
+
+  procedure, nopass, public :: error
+    !! Log to error channel
+
+  procedure, nopass, public :: panic
+    !! Log to panic channel (fortran_unit 0, always)
+
+  procedure, nopass, public :: reset
+    !! Reset all log channels (No more logging)
+  
+  procedure, nopass, public :: flush
+    !! Flush all log channels (empty buffers)
+
+  procedure, nopass, public :: add_stdout
+    !! Add the C++ ```std::cout``` output stream to each logchannel 
+
+  procedure, nopass, public :: set_stdout
+    !! Replace each logchannels streams with the C++ ```std::cout``` output stream
+
+  procedure, nopass, public :: add_fortran_unit
+    !! Add a given fortran unit as output stream to each logchannel 
+  
+  procedure, nopass, public :: set_fortran_unit
+    !! Replace each logchannel streams with a given fortran unit
+
+  procedure, nopass, public :: add_file
+    !! Add a given file as output stream to each logchannel.
+    !!
+    !! The file will be (re)created.
+
+  procedure, nopass, public :: set_file
+    !! Replace each logchannels streams with a given file
+    !!
+    !! The file will be (re)created.
+
+  procedure, nopass, public :: info_channel
+    !! Return the info [[fckit_logchannel(type)]]
+
+  procedure, nopass, public :: warning_channel
+    !! Return the warning [[fckit_logchannel(type)]]
+
+  procedure, nopass, public :: error_channel
+    !! Return the error [[fckit_logchannel(type)]]
+
+  procedure, nopass, public :: debug_channel
+    !! Return the debug [[fckit_logchannel(type)]]
+
+end type fckit_log_type
+
+type(fckit_log_type) :: fckit_log
+  !! Instance of [[fckit_log_module:fckit_log_type(type)]]
+
+!------------------------------------------------------------------------------
+
+type, extends(fckit_object) :: fckit_logchannel
+  !! Log channel (any of info, warning, error, debug)
+  !!
+  !! Wraps ```eckit::Channel```. This channel can be passed as
+  !! arguments to functions that expect a ```std::ostream``` to log to.
 contains
   procedure, public :: delete
-end type
-
-
-!------------------------------------------------------------------------------
-! Logger singleton
-type :: fckit_log_type
-  integer :: SIMPLE = 0
-  integer :: PREFIX = 1
-  integer :: TIMESTAMP = 2
-contains
-
-  ! subroutines
-  procedure, nopass, public :: debug
-  procedure, nopass, public :: info
-  procedure, nopass, public :: warning
-  procedure, nopass, public :: error
-  procedure, nopass, public :: panic
-  procedure, nopass, public :: reset
-  procedure, nopass, public :: flush
-  procedure, nopass, public :: add_stdout
-  procedure, nopass, public :: set_stdout
-  procedure, nopass, public :: add_fortran_unit
-  procedure, nopass, public :: set_fortran_unit
-  procedure, nopass, public :: add_file
-  procedure, nopass, public :: set_file
-
-  ! functions
-  procedure, nopass, public :: info_channel
-  procedure, nopass, public :: warning_channel
-  procedure, nopass, public :: error_channel
-  procedure, nopass, public :: debug_channel
-
-end type
-type(fckit_log_type) :: fckit_log
-type(fckit_log_type) :: log
-!------------------------------------------------------------------------------
-
-interface
-  ! void fckit__log_debug(char *msg, int newl, int flush)
-  subroutine fckit__log_debug(msg,newl,flush) bind(c)
-    use, intrinsic :: iso_c_binding , only : c_char, c_int
-    character(kind=c_char), dimension(*) :: msg
-    integer(c_int), value :: newl
-    integer(c_int), value :: flush
-  end subroutine
-  ! void fckit__log_info(char *msg, int newl, int flush)
-  subroutine fckit__log_info(msg,newl,flush) bind(c)
-    use, intrinsic :: iso_c_binding , only : c_char, c_int
-    character(kind=c_char), dimension(*) :: msg
-    integer(c_int), value :: newl
-    integer(c_int), value :: flush
-  end subroutine
-  ! void fckit__log_warning(char *msg, int newl, int flush)
-  subroutine fckit__log_warning(msg,newl,flush) bind(c)
-    use, intrinsic :: iso_c_binding , only : c_char, c_int
-    character(kind=c_char), dimension(*) :: msg
-    integer(c_int), value :: newl
-    integer(c_int), value :: flush
-  end subroutine
-  ! void fckit__log_error(char *msg, int newl, int flush)
-  subroutine fckit__log_error(msg,newl,flush) bind(c)
-    use, intrinsic :: iso_c_binding , only : c_char, c_int
-    character(kind=c_char), dimension(*) :: msg
-    integer(c_int), value :: newl
-    integer(c_int), value :: flush
-  end subroutine
-  subroutine fckit__log_add_fortran_unit(unit,style) bind(c)
-    use, intrinsic :: iso_c_binding, only : c_int
-    integer(c_int), value :: unit
-    integer(c_int), value :: style
-  end subroutine
-  subroutine fckit__log_set_fortran_unit(unit,style) bind(c)
-    use, intrinsic :: iso_c_binding, only : c_int
-    integer(c_int), value :: unit
-    integer(c_int), value :: style
-  end subroutine
-  subroutine fckit__log_add_file(path,style) bind(c)
-    use, intrinsic :: iso_c_binding , only : c_char, c_int
-    character(kind=c_char), dimension(*) :: path
-    integer(c_int), value :: style
-  end subroutine
-  subroutine fckit__log_set_file(path,style) bind(c)
-    use, intrinsic :: iso_c_binding , only : c_char, c_int
-    character(kind=c_char), dimension(*) :: path
-    integer(c_int), value :: style
-  end subroutine
-  subroutine fckit__log_add_stdout(style) bind(c)
-    use, intrinsic :: iso_c_binding , only : c_int
-    integer(c_int), value :: style
-  end subroutine
-  subroutine fckit__log_set_stdout(style) bind(c)
-    use, intrinsic :: iso_c_binding , only : c_int
-    integer(c_int), value :: style
-  end subroutine
-  subroutine fckit__log_reset() bind(c)
-  end subroutine
-  subroutine fckit__log_flush() bind(c)
-  end subroutine
-  function fckit__log_info_channel() bind(c)
-    use, intrinsic :: iso_c_binding, only : c_ptr
-    type(c_ptr) :: fckit__log_info_channel
-  end function
-  function fckit__log_warning_channel() bind(c)
-    use, intrinsic :: iso_c_binding, only : c_ptr
-    type(c_ptr) :: fckit__log_warning_channel
-  end function
-  function fckit__log_error_channel() bind(c)
-    use, intrinsic :: iso_c_binding, only : c_ptr
-    type(c_ptr) :: fckit__log_error_channel
-  end function
-  function fckit__log_debug_channel() bind(c)
-    use, intrinsic :: iso_c_binding, only : c_ptr
-    type(c_ptr) :: fckit__log_debug_channel
-  end function
-
-end interface
+    !! Delete internal C++ pointer
+end type fckit_logchannel
 
 !========================================================
 contains
@@ -137,7 +114,13 @@ subroutine debug(msg,newl,flush)
   use, intrinsic :: iso_c_binding
   use fckit_c_interop_module, only : c_str_right_trim
   character(kind=c_char,len=*), intent(in) :: msg
-  logical, intent(in), optional :: newl, flush
+    !! Message to be logged
+  logical, intent(in), optional :: newl
+    !! Add newline character (```\n```) after message.
+    !! Default ```.true.```
+  logical, intent(in), optional :: flush
+    !! Flush channel after message.
+    !! Default ```.false.```
   integer :: opt_newl, opt_flush
   opt_newl  = 1 ; if( present(newl) ) then; if( .not. newl ) opt_newl  = 0; endif
   opt_flush = 0 ; if( present(flush)) then; if(      flush ) opt_flush = 1; endif
@@ -149,7 +132,13 @@ subroutine info(msg,newl,flush)
   use, intrinsic :: iso_c_binding
   use fckit_c_interop_module, only : c_str_right_trim
   character(kind=c_char,len=*), intent(in) :: msg
-  logical, intent(in), optional :: newl, flush
+    !! Message to be logged
+  logical, intent(in), optional :: newl
+    !! Add newline character (```\n```) after message.
+    !! Default ```.true.```
+  logical, intent(in), optional :: flush
+    !! Flush channel after message.
+    !! Default ```.false.```
   integer :: opt_newl, opt_flush
   opt_newl  = 1 ; if( present(newl) ) then; if( .not. newl ) opt_newl  = 0; endif
   opt_flush = 0 ; if( present(flush)) then; if(      flush ) opt_flush = 1; endif
@@ -161,7 +150,13 @@ subroutine warning(msg,newl,flush)
   use, intrinsic :: iso_c_binding
   use fckit_c_interop_module, only : c_str_right_trim
   character(kind=c_char,len=*), intent(in) :: msg
-  logical, intent(in), optional :: newl, flush
+    !! Message to be logged
+  logical, intent(in), optional :: newl
+    !! Add newline character (```\n```) after message.
+    !! Default ```.true.```
+  logical, intent(in), optional :: flush
+    !! Flush channel after message.
+    !! Default ```.false.```
   integer :: opt_newl, opt_flush
   opt_newl  = 1 ; if( present(newl) ) then; if( .not. newl ) opt_newl  = 0; endif
   opt_flush = 0 ; if( present(flush)) then; if(      flush ) opt_flush = 1; endif
@@ -173,7 +168,13 @@ subroutine error(msg,newl,flush)
   use, intrinsic :: iso_c_binding
   use fckit_c_interop_module, only : c_str_right_trim
   character(kind=c_char,len=*), intent(in) :: msg
-  logical, intent(in), optional :: newl, flush
+    !! Message to be logged
+  logical, intent(in), optional :: newl
+    !! Add newline character (```\n```) after message.
+    !! Default ```.true.```
+  logical, intent(in), optional :: flush
+    !! Flush channel after message.
+    !! Default ```.false.```
   integer :: opt_newl, opt_flush
   opt_newl  = 1 ; if( present(newl) ) then; if( .not. newl ) opt_newl  = 0; endif
   opt_flush = 0 ; if( present(flush)) then; if(      flush ) opt_flush = 1; endif
@@ -184,6 +185,7 @@ end subroutine
 subroutine panic(msg)
   use, intrinsic :: iso_c_binding
   character(kind=c_char,len=*), intent(in) :: msg
+    !! Message to be logged
   write(0,'(A)') msg
 end subroutine
 
@@ -192,7 +194,13 @@ subroutine add_fortran_unit(unit,style)
   use, intrinsic :: iso_c_binding
   use fckit_c_interop_module
   integer(c_int), intent(in) :: unit
+    !! Fortran unit
   integer(c_int), intent(in), optional :: style
+    !! Style to prefix the stream with. Options are:
+    !!
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):SIMPLE(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):PREFIX(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):TIMESTAMP(variable)]]
   integer(c_int) :: opt_style
   opt_style = fckit_log%PREFIX
   if( present( style ) ) opt_style = style
@@ -204,7 +212,13 @@ subroutine set_fortran_unit(unit,style)
   use, intrinsic :: iso_c_binding
   use fckit_c_interop_module
   integer(c_int), intent(in) :: unit
+    !! Fortran unit
   integer(c_int), intent(in), optional :: style
+    !! Style to prefix the stream with. Options are:
+    !!
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):SIMPLE(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):PREFIX(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):TIMESTAMP(variable)]]
   integer(c_int) :: opt_style
   opt_style = fckit_log%PREFIX
   if( present( style ) ) opt_style = style
@@ -215,7 +229,13 @@ subroutine add_file(path,style)
   use, intrinsic :: iso_c_binding
   use fckit_c_interop_module
   character(len=*), intent(in) :: path
+    !! Path to file. File will be (re)created
   integer(c_int), intent(in), optional :: style
+    !! Style to prefix the stream with. Options are:
+    !!
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):SIMPLE(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):PREFIX(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):TIMESTAMP(variable)]]
   integer(c_int) :: opt_style
   opt_style = fckit_log%PREFIX
   if( present( style ) ) opt_style = style
@@ -227,7 +247,13 @@ subroutine set_file(path,style)
   use, intrinsic :: iso_c_binding
   use fckit_c_interop_module
   character(len=*), intent(in) :: path
+    !! Path to file. File will be (re)created
   integer(c_int), intent(in), optional :: style
+    !! Style to prefix the stream with. Options are:
+    !!
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):SIMPLE(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):PREFIX(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):TIMESTAMP(variable)]]
   integer(c_int) :: opt_style
   opt_style = fckit_log%PREFIX
   if( present( style ) ) opt_style = style
@@ -237,6 +263,11 @@ end subroutine
 subroutine add_stdout(style)
   use, intrinsic :: iso_c_binding
   integer(c_int), intent(in), optional :: style
+    !! Style to prefix the stream with. Options are:
+    !!
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):SIMPLE(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):PREFIX(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):TIMESTAMP(variable)]]
   integer(c_int) :: opt_style
   opt_style = fckit_log%PREFIX
   if( present( style ) ) opt_style = style
@@ -247,6 +278,11 @@ end subroutine
 subroutine set_stdout(style)
   use, intrinsic :: iso_c_binding
   integer(c_int), intent(in), optional :: style
+    !! Style to prefix the stream with. Options are:
+    !!
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):SIMPLE(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):PREFIX(variable)]]
+    !! - [[fckit_log_module:fckit_log(variable)]]%[[fckit_log_type(type):TIMESTAMP(variable)]]
   integer(c_int) :: opt_style
   opt_style = fckit_log%PREFIX
   if( present( style ) ) opt_style = style
@@ -263,31 +299,31 @@ subroutine flush()
 end subroutine
 
 function info_channel() result(channel)
-  type(logchannel) :: channel
+  type(fckit_logchannel) :: channel
   call channel%reset_c_ptr( fckit__log_info_channel() )
 end function
 
 
 function warning_channel() result(channel)
-  type(logchannel) :: channel
+  type(fckit_logchannel) :: channel
   call channel%reset_c_ptr( fckit__log_warning_channel() )
 end function
 
 
 function error_channel() result(channel)
-  type(logchannel) :: channel
+  type(fckit_logchannel) :: channel
   call channel%reset_c_ptr( fckit__log_error_channel() )
 end function
 
 
 function debug_channel() result(channel)
-  type(logchannel) :: channel
+  type(fckit_logchannel) :: channel
   call channel%reset_c_ptr( fckit__log_debug_channel() )
 end function
 
 
 subroutine delete(this)
-  class(logchannel), intent(inout) :: this
+  class(fckit_logchannel), intent(inout) :: this
   ! do nothing
 end subroutine
 
