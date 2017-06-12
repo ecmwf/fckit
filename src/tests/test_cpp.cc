91547a61ce05682ca35871e8c523ec6d5c7b57ad
Willem Deconinck <willem.deconinck@ecmwf.int>
2017-06-12 09:44:58 +0100

    Prove OOPS-8 with unit-test

--- src/tests/test_cpp.cc
+++ src/tests/test_cpp.cc
@@ -8,10 +8,41 @@ using namespace fckit;
 extern "C" {
   void run();
 }
+
+
+#include <memory>
+#include "eckit/log/OStreamTarget.h"
+#include "eckit/log/PrefixTarget.h"
+namespace fckit {
+namespace test {
+class Libdummy {
+public:
+  static Libdummy& instance() {
+    static Libdummy lib;
+    return lib;
+  }
+  void finalise() {
+    traceChannel_.reset( new eckit::Channel() );
+  }
+  eckit::Channel& traceChannel() {
+    if( traceChannel_ ) return *traceChannel_;
+    traceChannel_.reset( new eckit::Channel(
+        new eckit::PrefixTarget("FCKIT_TRACE",new eckit::OStreamTarget(eckit::Log::info()))));
+    return *traceChannel_;
+}
+private:
+  std::unique_ptr<eckit::Channel> traceChannel_;
+};
+}
+}
+
+
 int main(int argc, char* argv[])
 {
   Main::initialize(argc,argv);
   Main::instance().taskID(eckit::mpi::comm().rank());
+
+  test::Libdummy::instance().traceChannel() << "before configure of logging" << std::endl;
 
   if( Main::instance().taskID() == 0 ) {
     Log::setFortranUnit(Log::output_unit(),Log::TIMESTAMP);
@@ -19,8 +50,12 @@ int main(int argc, char* argv[])
   } else {
     Log::reset();
   }
+  test::Libdummy::instance().traceChannel() << "after configure of logging" << std::endl;
+
+
   run();
   Log::debug<Libfckit>() << "message from Libfckit" << std::endl;
+  test::Libdummy::instance().finalise();
   Main::finalize();
   return 0;
 }
