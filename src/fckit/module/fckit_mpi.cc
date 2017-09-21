c59e4e7cd4ad57c418033525a1ebc3bed6a66413
Willem Deconinck <willem.deconinck@ecmwf.int>
2017-09-21 10:26:48 +0100

    Compatibility with eckit 0.17 hotfixes

--- src/fckit/module/fckit_mpi.cc
+++ src/fckit/module/fckit_mpi.cc
@@ -6,8 +6,7 @@
 #undef ECKIT_VERSION_INT
 #endif
 #define ECKIT_VERSION_INT (ECKIT_MAJOR_VERSION * 10000 \
-                         + ECKIT_MINOR_VERSION * 100 \
-                         + ECKIT_PATCH_VERSION)
+                         + ECKIT_MINOR_VERSION * 100 )
 
 using eckit::mpi::Comm;
 
