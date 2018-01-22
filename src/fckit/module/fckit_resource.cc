9bf7bb80b3771c53269a999e4bd47939cfd712c0
Willem Deconinck <willem.deconinck@ecmwf.int>
2018-01-22 16:16:18 +0000

    FCKIT-1 Update copyright notice

--- src/fckit/module/fckit_resource.cc
+++ src/fckit/module/fckit_resource.cc
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2013-2017 ECMWF.
+ * (C) Copyright 2013 ECMWF.
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -8,8 +8,7 @@
  * does it submit to any jurisdiction.
  */
 
-#ifndef fckit_resource_h
-#define fckit_resource_h
+#pragma once
 
 #include <string.h>
 #include "eckit/config/Resource.h"
@@ -52,6 +51,3 @@ extern "C"
     return SUCCESS;
   }
 }
-
-#endif
-
