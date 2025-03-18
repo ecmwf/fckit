ba1da1a856bb2a48d03bfad8840d919509e6b56f
Willem Deconinck <willem.deconinck@ecmwf.int>
2025-03-18 10:17:10 +0100

    Fix for #56, propagating flags to fckit_test_downstream_fypp (#58)
    * Fix for #56, propagating flags to fckit_test_downstream_fypp

--- src/tests/test_downstream_fypp/compile_flags.cmake
+++ src/tests/test_downstream_fypp/compile_flags.cmake
@@ -1,7 +1,11 @@
 # File used to change or override compile flags
 
-set( DOWNSTREAM_Fortran_FLAGS                "-DDOWNSTREAM_Fortran_FLAGS='\"DOWNSTREAM_Fortran_FLAGS\"' ${CMAKE_Fortran_FLAGS}" )
+
+message("----------- CMAKE_Fortran_FLAGS   = ${CMAKE_Fortran_FLAGS}")
+message("----------- ECBUILD_Fortran_FLAGS = ${ECBUILD_Fortran_FLAGS}")
+
+set( DOWNSTREAM_Fortran_FLAGS                "-DDOWNSTREAM_Fortran_FLAGS='\"DOWNSTREAM_Fortran_FLAGS\"' ${CMAKE_Fortran_FLAGS} ${ECBUILD_Fortran_FLAGS}" )
 set( DOWNSTREAM_Fortran_FLAGS_RELWITHDEBINFO "-DDOWNSTREAM_Fortran_FLAGS_RELWITHDEBINFO='\"DOWNSTREAM_Fortran_FLAGS_RELWITHDEBINFO\"'" )
 
 set_source_files_properties( downstream.fypp          PROPERTIES COMPILE_FLAGS          "-DDOWNSTREAM_COMPILE_FLAGS='\"DOWNSTREAM_COMPILE_FLAGS\"'" )
-set_source_files_properties( downstream_override.fypp PROPERTIES OVERRIDE_COMPILE_FLAGS "-DDOWNSTREAM_COMPILE_FLAGS='\"DOWNSTREAM_OVERRIDE_COMPILE_FLAGS\"' ${CMAKE_Fortran_FLAGS}" )
+set_source_files_properties( downstream_override.fypp PROPERTIES OVERRIDE_COMPILE_FLAGS "-DDOWNSTREAM_COMPILE_FLAGS='\"DOWNSTREAM_OVERRIDE_COMPILE_FLAGS\"' ${CMAKE_Fortran_FLAGS} ${ECBUILD_Fortran_FLAGS}" )
